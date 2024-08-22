37889917299fbba1b5a3bb1df51f768381697cf3
ntp-bi <bintp22122002@gmail.com>
2024-08-22 20:47:38 +0700

    make UI header P.1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { SearchIcon, EllipsisVerticalIcon } from "../icons/Icon";
+import { configPath } from "../../config/configPath";
+
+import logo from "../../assets/images/image.png";
 
 import "./header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+    return (
+        <header className="header">
+            <nav className="header__container">
+                <Link to={configPath.home} className="header__left">
+                    <img src={logo} alt="" className="header__left__image" />
+                </Link>
+                <div className="header__center">
+                    <input type="text" className="header__center__input" />
+                    <button className="header__center__btn--clear">
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        icon={faSpinner}
+                        className="header__center__btn--loading"
+                    />
 
-export default Header
+                    <button className="header__center__btn--search">
+                        <SearchIcon />
+                    </button>
+                </div>
+                <div className="header__right">
+                    <button>Upload</button>
+                    <button>Login</button>
+                    <EllipsisVerticalIcon />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
